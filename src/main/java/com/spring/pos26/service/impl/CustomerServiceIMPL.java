8716a372c4bcd3c1f4e57b9cd3cecee4709713f8
perera2001 <nandunperera2001@gmail.com>
2026-01-30 00:25:56 +0530

    delete method create

--- src/main/java/com/spring/pos26/service/impl/CustomerServiceIMPL.java
+++ src/main/java/com/spring/pos26/service/impl/CustomerServiceIMPL.java
@@ -79,4 +79,16 @@ public class CustomerServiceIMPL implements CustomerService {
         }
         return customerDTOList;
     }
+
+    @Override
+    public String deleteCustomer(int customerId) {
+        if(customerRepo.existsById(customerId)) {
+            customerRepo.deleteById(customerId);
+            return "Deleted ";
+
+        }else{
+            throw  new RuntimeException("Customer not found");
+        }
+
+    }
 }
